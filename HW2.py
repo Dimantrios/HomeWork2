354a3ebf45c8dbf42f86faf881866b2fc5607b6c
ArtLast <dimantrios@online.ua>
2022-10-21 17:34:51 +0300

    HomeWork2

--- HW2.py
+++ HW2.py
@@ -3,14 +3,13 @@ b = int(input('Please, enter length side b: '))
 c = int(input('Please, enter length side c: '))
 
 i = [a, b, c]
-
 P = a + b + c
 
-if a == 0:
+if a <= 0:
     print('it is impossible to calculate the perimeter')
-elif b == 0:
+elif b <= 0:
     print('it is impossible to calculate the perimeter')
-elif c == 0:
+elif c <= 0:
     print('it is impossible to calculate the perimeter')
 elif a > 0:
     print('P =', P)
@@ -19,15 +18,11 @@ elif b > 0:
 elif c > 0:
     print('P =', P)
 
+# Не успел найти как сравнивать каждый элемент из списка или перечень переменных с нолём
 
-
-if a == 0:
-    print('no such triangle exists')
-elif b == 0:
-    print('no such triangle exists')
-elif c == 0:
-    print('no such triangle exists')
-elif P > 20:
+if 0 < a and 0 < b and 0 < c and P > 20:
     print(max(i))
-elif P < 10:
+elif 0 < a and 0 < b and 0 < c and P < 10:
     print(min(i))
+
+
